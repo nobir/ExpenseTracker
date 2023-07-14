cad64f13032487a593a689a22bb0dc46cfacc8eb
Mahim Sarkar <106544262+MahimSarkar42042@users.noreply.github.com>
2023-07-14 22:36:29 +0600

    Update delete_expense.py
    Worked on Update delete_expense.py

--- functions/delete_expense.py
+++ functions/delete_expense.py
@@ -1,2 +1,15 @@
 def delete_expense(expense_data):
-    NotImplementedError()
+
+    expense_id = int(input("Enter the expense ID to delete: "))
+
+    for expense in expense_data:
+        if expense['ID'] == expense_id:
+         # Check if the ID of the current expense matches the expense_id to be deleted
+            
+            expense_data.remove(expense)
+            # If a match is found, remove the expense from the expense_data list
+
+            print("Expense deleted successfully!")
+            return
+            
+    print("Expense not found.")
