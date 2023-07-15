9d0a692c89beb7a84552f445ff37db3cf5e558d0
Nobir <nobirfreelancer@gmail.com>
2023-07-15 17:05:55 +0600

    Added validation rule to category

--- functions/add_category.py
+++ functions/add_category.py
@@ -1,4 +1,16 @@
 def add_category(categories):
-    category = input("Enter the new category: ")
-    categories.append(category)
-    print("Category added successfully!")
+    while True:
+        category = input("Enter the new category: ")
+
+        # Validation rules
+        if not category:
+            print("Category cannot be empty. Please enter a category.")
+        elif len(category) < 3:
+            print("Category must be at least 3 characters long.")
+        elif category in categories:
+            print("Category added successfully!")
+            break
+        else:
+            categories.append(category)
+            print("Category added successfully!")
+            break
